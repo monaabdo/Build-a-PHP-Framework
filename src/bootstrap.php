653cc42412397416d8c2be35c1a1948b3e459346
monaabdo88 <monaabdo88@gmail.com>
2026-09-09 22:38:20 +0300

    Dependency Injection

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,25 +1,32 @@
 <?php
+
 declare(strict_types=1);
 
 use GuzzleHttp\Psr7\ServerRequest;
 use HttpSoft\Emitter\SapiEmitter;
-use League\Route\RouteCollection;
 use League\Route\Router;
+
 use App\Controllers\HomeController;
 use App\Controllers\ProductsController;
 
-ini_set("display_errors",'1');
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-require dirname(__DIR__)."/vendor/autoload.php";
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 $request = ServerRequest::fromGlobals();
 
 $router = new Router();
 
-$router->get("/",[HomeController::class,"index"]);
+$router->get('/', [HomeController::class, 'index']);
 
-$router->get("/products",[ProductsController::class,"index"]);
-$router->get("/product/{id:number}",[ProductsController::class,"show"]);
+$router->get('/products', [ProductsController::class, 'index']);
+
+$router->get('/product/{id:number}', [ProductsController::class, 'show']);
+
 $response = $router->dispatch($request);
-$emitter = new SapiEmitter;
+
+$emitter = new SapiEmitter();
+
 $emitter->emit($response);
